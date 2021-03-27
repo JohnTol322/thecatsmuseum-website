7c31fea0f16311b49dbd85d87959a5cfbcd99773
John Tol <johntol322@gmail.com>
2021-03-27 11:41:26 +0100

    home pagina verplaatst naar pages map

---
+++ website/pages/home/index.php
@@ -0,0 +1,9 @@
+<?php include "../../php-includes/html-head.php" ?>
+<title>TheCatsMuseum | | Home</title>
+<?php include "../../php-includes/header.php" ?>
+<main id="home-main">
+      <div class="page-box"></div>
+    </main>
+    <footer></footer>
+  </body>
+</html>
--- website/php-includes/header.php
+++ website/php-includes/header.php
@@ -7,7 +7,7 @@
       <h1 id="title">The Cats Museum</h1>
     </div>
     <ul id="nav">
-      <li><a href="../../">Home</a></li>
+      <li><a href="../home/">Home</a></li>
       <li><a href="../pagina2">pagina 2</a></li>
       <li><a href="../pagina3">pagina 3</a></li>
       <li><a href="../contact/">Contact</a></li>
