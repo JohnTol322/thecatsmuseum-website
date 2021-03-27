188b4fd6b0367b908a9d4249e5239c01659e4728
John Tol <johntol322@gmail.com>
2021-03-27 17:08:39 +0100

    homepage veranderd en contact pagina toegevoegd

--- website/pages/contact/index.php
+++ website/pages/contact/index.php
@@ -5,7 +5,31 @@
 <?php include "../../php-includes/header.php" ?>
 
     <main id="contact-main">
-      <div class="page-box"></div>
+      <div class="page-box">
+        <h1>Contact</h1>
+        <hr>
+        <p>Bezoek alleen op afspraak, maak uw afspraak hier via de volgende platformen: </p>
+        <hr>
+        <div id="contact-gegevens">
+          <div class="flex">
+            <label for="email">E-mail: </label>
+            <a href="mailto:schildersbedrijfjohantol@gmail.com?subject=The Cats Museum">schildersbedrijfjohantol@gmail.com</a>
+          </div>
+          <br>
+          <div class="flex">
+            <label for="tel">Telefoon: </label>
+            <a href="tel:+31 6 43 08 66 32">+31 6 43 08 66 32</a>
+          </div>
+          <br>
+          <div>
+            <h6 id="sm-message">wij zijn ook op social media te bereiken:</h6>
+            <div class="flex">
+            <a href="https://www.instagram.com/the_cats_museum/" target="_blank"><img src="../../images/instagram.png" alt="instagram" class="sm-icon"></a>
+              <a href="https://www.facebook.com/Onewaywind68" target="_blank"><img src="../../images/facebook.png" alt="facebook" class="sm-icon"></a>
+            </div>
+          </div>
+        </div>
+      </div>
     </main>
   </body>
 </html>
--- website/pages/home/index.php
+++ website/pages/home/index.php
@@ -3,7 +3,14 @@
 <?php include "../../php-includes/header.php" ?>
 <main id="home-main">
       <div class="page-box">
+          <div style="display: flex; justify-content: space-around;">
+          <h1 id="home-title">Beste Bezoekers, </h1>
+          </div>
+          <hr>
+          <div class="centreer">
           <img src="../../images/homeimage.jpg" alt="image" id="homeimage">
+          </div>
+          <br>
           <p id="homepage-text"></p>
       </div>
     </main>
