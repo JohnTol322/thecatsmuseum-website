c3895cad1a6aa17075bcf21977202257434d0cab
John Tol <johntol322@gmail.com>
2021-03-27 11:58:40 +0100

    fetch geeft geen error meer

--- website/js/fetch-homepage_text.js
+++ website/js/fetch-homepage_text.js
@@ -1,5 +1,7 @@
 let file = "../../txt/homepage_text.txt"
 
-fetch (file)
-.then(x => x.text())
-.then(y => document.getElementById("homepage-text").innerHTML = y);
+if(document.getElementById("homepage-text") != null){
+    fetch (file)
+    .then(x => x.text())
+    .then(y => document.getElementById("homepage-text").innerHTML = y);
+}
