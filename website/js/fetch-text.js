83f3433f6a68cbb696256486b3e4afe0f23e0cbc
John Tol <johntol322@gmail.com>
2021-04-05 10:02:50 +0200

    over ons pagina, foto toegevoegd en tekst gefetcht

--- website/js/fetch-text.js
+++ website/js/fetch-text.js
@@ -1,7 +1,15 @@
-let file = "../../txt/homepage_text.txt"
+let hometext = "../../txt/homepage_text.txt"
 
 if(document.getElementById("homepage-text") != null){
-    fetch (file)
+    fetch (hometext)
     .then(x => x.text())
     .then(y => document.getElementById("homepage-text").innerHTML = y);
+}
+
+let abouttext = "../../txt/about_intro_text.txt"
+
+if(document.getElementById("about-text") != null){
+    fetch (abouttext)
+    .then(x => x.text())
+    .then(y => document.getElementById("about-text").innerHTML = y);
 }
--- website/pages/about/index.php
+++ website/pages/about/index.php
@@ -6,6 +6,14 @@
 
 <main id="about-main">
     <div class="page-box">
-
+    <div style="display: flex; justify-content: space-around;">
+          <h1 id="home-title">Over Ons</h1>
+          </div>
+          <hr>
+          <div class="centreer">
+          <img src="../../images/aboutimage.jpg" alt="image" id="aboutimage">
+          </div>
+          <br>
+          <p id="about-text"></p>
     </div>
 </main>
