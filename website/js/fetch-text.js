aec04943ca6448fff626081b531f396e37dc26c8
John Tol <johntol322@gmail.com>
2021-04-10 12:14:46 +0200

    tekst bestanden worden gefetched

--- website/js/fetch-text.js
+++ website/js/fetch-text.js
@@ -1,15 +1,24 @@
 let hometext = "../../txt/homepage_text.txt"
-
-if(document.getElementById("homepage-text") != null){
-    fetch (hometext)
-    .then(x => x.text())
-    .then(y => document.getElementById("homepage-text").innerHTML = y);
-}
-
 let abouttext = "../../txt/about_intro_text.txt"
+let pietveerman = "../../txt/pietveerman.txt"
+let arnoldmuhren = "../../txt/arnoldmuhren.txt"
+let harmenveerman = "../../txt/harmenveerman.txt"
+let keesbaars = "../../txt/keesbaars.txt"
+let johantol = "../../txt/johantol.txt"
+let nieuwsmuziekwijk = "../../txt/nieuwsmuziekwijk.txt"
 
-if(document.getElementById("about-text") != null){
-    fetch (abouttext)
-    .then(x => x.text())
-    .then(y => document.getElementById("about-text").innerHTML = y);
+fetchText("homepage-text", hometext);
+fetchText("about-text", johantol);
+fetchText("pietveerman-text", pietveerman);
+fetchText("arnoldmuhren-text", arnoldmuhren);
+fetchText("harmenveerman-text", harmenveerman);
+fetchText("keesbaars-text", keesbaars);
+fetchText("nieuws-text", nieuwsmuziekwijk);
+
+function fetchText(target, file){
+    if(document.getElementById(target) != null){
+        fetch (file)
+        .then(x => x.text())
+        .then(y => document.getElementById(target).innerHTML = y);
+    }
 }
