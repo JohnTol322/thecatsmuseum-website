b1a6e7b13afb10f3800c97301a70c7753dfa2c04
John Tol <johntol322@gmail.com>
2021-06-02 12:29:19 +0200

    styling voor kleiner scherm

--- website/php-includes/header.php
+++ website/php-includes/header.php
@@ -13,4 +13,18 @@
       <li><a href="../gasten/">Gasten</a></li>
       <li><a href="../contact/">Contact</a></li>
     </ul>
+    <a href="#" class="toggle-button">
+      <span class="bar"></span>
+      <span class="bar"></span>
+      <span class="bar"></span>
+    </a>
   </header>
+  <div class="togglenav">
+  <ul id="mini-nav">
+      <li><a href="../home/">Home</a></li>
+      <li><a href="../about/">Over Ons</a></li>
+      <li><a href="../nieuws/">Nieuws</a></li>
+      <li><a href="../gasten/">Gasten</a></li>
+      <li><a href="../contact/">Contact</a></li>
+    </ul>
+  </div>
